f33bf00fb9b0870fac529b48f2eea802a3fe2bab
Charlotte Lee <charlotteuxd@gmail.com>
2024-12-05 13:14:52 +0800

    Image animation
    Updated with image animation.

--- menu.js
+++ menu.js
@@ -1,11 +1,13 @@
 const toggleTag = document.querySelector('a.toggle-nav');
 const mainTag = document.querySelector('main');
+const navTag = document.querySelector('nav');
 
 // When I click the toggle tag, toggle a class of open on the main tag
 // and if it's open, make the toggle tag say close
 // and if not, make the toggle tag say open
 toggleTag.addEventListener('click', function () {
   mainTag.classList.toggle('open');
+  navTag.classList.toggle('open');
 
   if (mainTag.classList.contains('open')) {
     toggleTag.innerHTML = `<img src="assets/close.svg"> Close`;
